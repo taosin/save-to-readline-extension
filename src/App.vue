18cfc3e5ac19f96f8a62001b1dcb4af7b818fe22
iMocco <taoxin167@gmail.com>
2017-08-30 16:50:41 +0800

    完成Readline初步保存和读取

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
 	<div id="app">
-		<router-view></router-view>
+		<Index></Index>
+		<!-- <Setting></Setting> -->
 	</div>
 </template>
 
 <script>
-	var AV = require('leancloud-storage');
-	var APP_ID = '{{your appId}}';
-	var APP_KEY = '{{your appKey}}';
+	var APP_ID = 'your appid';
+	var APP_KEY = 'your appKey';
 	AV.init({
 		appId: APP_ID,
 		appKey: APP_KEY
 	});
+	// chrome.storage.sync.get("lelKey", function(data) {
+	// 	if(data.lelKey){
+	// 		var APP_KEY = '';
+	// 		var APP_ID = '';
+	// 		APP_ID = data.lelKey.split('|')[0];
+	// 		APP_KEY = data.lelKey.split('|')[1];
+	// 	}
+	// }) 
+	import Index from './index';
+	import Setting from './setting';
 	export default {
-		name: 'app'
+		components:{
+			Index,
+			Setting
+		},
+		name: 'app',
+		data(){
+			return{
+				hasKey: false
+			}
+		},
+		mounted(){
+		}
 	}
 </script>
 
 <style>
+	#app{
+		height: 100%;
+	}
 </style>
---
+++ src/setting.vue
@@ -0,0 +1,93 @@
+<!-- 绑定appid -->
+<template>
+	<div class="index">
+		<h1>绑定AppID和AppKey</h1>
+		<div class="main">
+			<div class="item">
+				<input type="text" placeholder="Your appId" v-model="appId">
+			</div>
+			<div class="item">
+				<input type="text" placeholder="Your appKey" v-model="appKey">
+			</div>
+			<div class="item">
+				<button @click="submit">提交</button>
+			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+	var AV = require('leancloud-storage');
+	export default {
+		name: 'index',
+		data () {
+			return {
+				appId:'',
+				appKey:''
+			}
+		},
+		mounted(){
+		},
+		methods:{
+			submit(){
+				if(!this.appId || !this.appKey){
+					alert('appId或appKey不能为空！');
+					return;
+				}
+				// chrome.storage.sync.set({'lelKey': this.appId +'|' + this.appKey},function(){alert("设置成功！");
+				// 	chrome.tabs.close();})
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.index{
+		width: 1000px;
+		margin: 0 auto;
+		padding: 20px;
+		overflow: hidden;
+	}
+	h1{
+		text-align: center;
+	}
+	.main{
+		border: 1px solid #ddd;
+		width: 400px;
+		height: 200px;
+		margin: 0 auto;
+		margin-top: 30px;
+		border-radius: 10px;
+		padding-top: 20px;
+		background: #fff;
+	}
+	.item{
+		text-align: center;
+		width: 100%;
+		padding: 10px 0;
+	}
+	input{
+		width: 80%;
+		height: 30px;
+		border: 1px solid #2d8cf0;
+		padding: 0 10px;
+	}
+	input:focus{
+		outline: none;
+	}
+	button{
+		width: 100px;
+		padding: 5px 10px;
+		background: #2d8cf0;
+		border: 1px solid #ddd;
+		color: #fff;
+		font-size: 16px;
+		cursor: pointer;
+	}
+	button:focus,
+	button:hover{
+		outline: none;
+		opacity: .8;
+	}
+</style>
